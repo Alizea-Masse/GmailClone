0ddf108538ea48c49ce3e210703e083042312156
Alizéa Massé <alizea777@hotmail.fr>
2023-03-28 16:15:49 +0200

    first commit create header

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './style.css'
+function Header() {
+  return (
+    <div className='Header'>
+        <h2>This is a header</h2>
+    </div>
+  )
+}
+
+export default Header

0a459d988ea2fd09b5c84388597427af35afd4b4
Alizéa Massé <alizea777@hotmail.fr>
2023-03-29 09:58:24 +0200

    header done

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,35 @@
 import React from 'react'
 import './style.css'
+import MenuIcon from '@mui/icons-material/Menu';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import AppsIcon from '@mui/icons-material/Apps';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import { Avatar } from '@mui/material';
 function Header() {
   return (
-    <div className='Header'>
-        <h2>This is a header</h2>
+    <div className='header'>
+        <div className="header__left">
+            <IconButton>
+                <MenuIcon />
+            </IconButton>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/a/ab/Gmail2020.logo.png" alt="Gmail Logo" />
+        </div>
+        <div className="header__middle">
+          <SearchIcon />
+          <input type="text" placeholder="Rechercher un mail" />
+          <ArrowDropDownIcon className="header__inputCaret" />
+        </div>
+        <div className="header__right">
+        <IconButton>
+                <AppsIcon />
+            </IconButton>
+            <IconButton>
+                <NotificationsNoneIcon />
+            </IconButton>
+            <Avatar />
+        </div>
     </div>
   )
 }
